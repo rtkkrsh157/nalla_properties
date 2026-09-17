587b0ecff77ae91cc0b96c0fcb04120419b900e3
Ratik Krishna <ratikkrishna15@gmail.com>
2026-09-17 09:16:46 +0530

    Configure Web3Forms environment variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
+
   output: 'standalone',
+
+  env: {
+    NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY:
+      process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY,
+  },
 }
 
 export default nextConfig
